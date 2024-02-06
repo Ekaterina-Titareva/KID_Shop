d3772239b1a231e2874b1e3f4c058f6b2565dfd4
“Elena <bachi.elena@gmail.com>
2024-02-07 00:22:24 +0300

    Добавила начальную разметку, логику открытия/закрытия popup корзины

--- src/index.js
+++ src/index.js
@@ -14,8 +14,25 @@ console.log(title);
 const button = document.querySelector('.user');
 const form = document.querySelector('#user_login');
 const popup = document.querySelector('.popup_user');
+const buttonBasket = document.querySelector('#basket');
+const buttonClose = document.querySelector('.btn-close');
+const popupBasket = document.querySelector('.popup-basket');
 
 button.addEventListener('click', () => {
   form.classList.add('open');
   popup.classList.add('popup_open');
 });
+
+buttonBasket.addEventListener('click', () => {
+  if (popupBasket.classList.contains('hidden')){
+    popupBasket.classList.remove('hidden');
+    popupBasket.classList.add('visible');
+  }
+})
+
+buttonClose.addEventListener('click', () => {
+  if (popupBasket.classList.contains('visible')){
+    popupBasket.classList.remove('visible');
+    popupBasket.classList.add('hidden');
+  }
+})
