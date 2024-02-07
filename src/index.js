b4759c11117bd5fa06a4efff6b6e7603e2a0071d
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-02-07 16:28:33 +0300

    added a slider

--- src/index.js
+++ src/index.js
@@ -19,3 +19,37 @@ button.addEventListener('click', () => {
   form.classList.add('open');
   popup.classList.add('popup_open');
 });
+
+
+// Слайдер
+// Список изображений и текущий индекс
+const images = [
+  require('./images/advert1.jpg'),
+  require('./images/advert2.jpg'),
+  require('./images/advert3.jpg'),
+];
+
+let currentImageIndex = 0;
+const imageElement = document.getElementById("ad");
+
+// Функция для отображения текущего изображения
+function displayCurrentImage() {
+	imageElement.src = images[currentImageIndex];
+}
+const prevButton = document.querySelector(".pre-btn");
+
+// Обработчик события для кнопки "Назад"
+prevButton.addEventListener("click", function() {
+	currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+	displayCurrentImage();
+});
+const nextButton = document.querySelector(".nxt-btn");
+
+// Обработчик события для кнопки "Вперед"
+nextButton.addEventListener("click", function() {
+	currentImageIndex = (currentImageIndex + 1) % images.length;
+	displayCurrentImage();
+});
+
+// Отображаем первое изображение при загрузке страницы
+displayCurrentImage();
